c7dbc4131089eb1fcae086f0025dea52bbcb4017
Manfred Michael <koskosanrider@gmail.com>
2024-07-08 20:40:25 +0700

    Add tts

--- cam_display.py
+++ cam_display.py
@@ -5,11 +5,106 @@ import numpy as np
 import concurrent.futures
 import time
 from ultralytics import YOLO
+from gtts import gTTS
+from playsound import playsound
+import os
 
 from http.client import IncompleteRead
  
 url='http://192.168.0.104/cam-hi.jpg'
 im=None
+
+if os.path.exists("output.mp3"):
+    os.remove("output.mp3")
+
+# Translation dictionary
+translations = {
+    'person': 'orang',
+    'bicycle': 'sepeda',
+    'car': 'mobil',
+    'motorcycle': 'motor',
+    'airplane': 'pesawat',
+    'bus': 'bus',
+    'train': 'kereta api',
+    'truck': 'truk',
+    'boat': 'perahu',
+    'traffic light': 'lampu lalu lintas',
+    'fire hydrant': 'hydrant',
+    'stop sign': 'rambu berhenti',
+    'parking meter': 'meteran parkir',
+    'bench': 'bangku',
+    'bird': 'burung',
+    'cat': 'kucing',
+    'dog': 'anjing',
+    'horse': 'kuda',
+    'sheep': 'domba',
+    'cow': 'sapi',
+    'elephant': 'gajah',
+    'bear': 'beruang',
+    'zebra': 'zebra',
+    'giraffe': 'jerapah',
+    'backpack': 'ransel',
+    'umbrella': 'payung',
+    'handbag': 'tas tangan',
+    'tie': 'dasi',
+    'suitcase': 'koper',
+    'frisbee': 'frisbee',
+    'skis': 'ski',
+    'snowboard': 'papan seluncur salju',
+    'sports ball': 'bola olahraga',
+    'kite': 'layang-layang',
+    'baseball bat': 'tongkat baseball',
+    'baseball glove': 'sarung tangan baseball',
+    'skateboard': 'papan luncur',
+    'surfboard': 'papan selancar',
+    'tennis racket': 'raket tenis',
+    'bottle': 'botol',
+    'wine glass': 'gelas anggur',
+    'cup': 'cangkir',
+    'fork': 'garpu',
+    'knife': 'pisau',
+    'spoon': 'sendok',
+    'bowl': 'mangkuk',
+    'banana': 'pisang',
+    'apple': 'apel',
+    'sandwich': 'roti lapis',
+    'orange': 'jeruk',
+    'broccoli': 'brokoli',
+    'carrot': 'wortel',
+    'hot dog': 'hot dog',
+    'pizza': 'pizza',
+    'donut': 'donat',
+    'cake': 'kue',
+    'chair': 'kursi',
+    'couch': 'sofa',
+    'potted plant': 'tanaman dalam pot',
+    'bed': 'tempat tidur',
+    'dining table': 'meja makan',
+    'toilet': 'toilet',
+    'tv': 'televisi',
+    'laptop': 'laptop',
+    'mouse': 'mouse',
+    'remote': 'remote',
+    'keyboard': 'keyboard',
+    'cell phone': 'ponsel',
+    'microwave': 'microwave',
+    'oven': 'oven',
+    'toaster': 'pemanggang roti',
+    'sink': 'wastafel',
+    'refrigerator': 'kulkas',
+    'book': 'buku',
+    'clock': 'jam',
+    'vase': 'vas',
+    'scissors': 'gunting',
+    'teddy bear': 'boneka beruang',
+    'hair drier': 'pengering rambut',
+    'toothbrush': 'sikat gigi'
+}
+
+# Function to translate detected objects
+def translate_labels(labels):
+    return [translations[label] if label in translations else label for label in labels]
+
  
 def run1():
     cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
@@ -45,13 +140,29 @@ def run2():
             results = model(im)  # Detect objects
 
             print('detection - drawing..')
+            detected_objects = []
             for result in results:
                 for box in result.boxes:
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
                     conf = box.conf[0]
                     label = model.names[int(box.cls[0])]
+
+                    detected_objects.append(label)
+
+
                     cv2.rectangle(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     cv2.putText(im, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            if len(detected_objects)>0:
+                detected_objects = translate_labels(detected_objects)
+                text = ",".join(detected_objects) 
+
+                tts = gTTS(text=text, lang='id')
+                tts.save("output.mp3")
+                playsound("output.mp3")
+                os.remove("output.mp3")
+
+                time.sleep(2)
 
             print('detection - showing..')
             cv2.imshow('detection', im)
